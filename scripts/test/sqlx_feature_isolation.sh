01dd7413152c9dee73301f952737d6c4d997c24c
xic <134991273+xicv@users.noreply.github.com>
2026-07-27 14:26:38 +0930

    test(deps): cover Orders service feature graphs

--- scripts/test/sqlx_feature_isolation.sh
+++ scripts/test/sqlx_feature_isolation.sh
@@ -56,16 +56,32 @@ orders_postgres=$(cargo tree --locked \
   -p orders-adapters \
   --no-default-features \
   --features postgres)
-require_contains "${orders_postgres}" "sqlx-postgres" "Orders PostgreSQL graph"
-require_absent "${orders_postgres}" "sqlx-sqlite" "Orders PostgreSQL graph"
-require_absent "${orders_postgres}" "libsqlite3-sys" "Orders PostgreSQL graph"
+require_contains "${orders_postgres}" "sqlx-postgres" "Orders PostgreSQL adapter graph"
+require_absent "${orders_postgres}" "sqlx-sqlite" "Orders PostgreSQL adapter graph"
+require_absent "${orders_postgres}" "libsqlite3-sys" "Orders PostgreSQL adapter graph"
 
 orders_sqlite=$(cargo tree --locked \
   -p orders-adapters \
   --no-default-features \
   --features sqlite)
-require_contains "${orders_sqlite}" "sqlx-sqlite" "Orders SQLite graph"
-require_contains "${orders_sqlite}" "libsqlite3-sys" "Orders SQLite graph"
-require_absent "${orders_sqlite}" "sqlx-postgres" "Orders SQLite graph"
+require_contains "${orders_sqlite}" "sqlx-sqlite" "Orders SQLite adapter graph"
+require_contains "${orders_sqlite}" "libsqlite3-sys" "Orders SQLite adapter graph"
+require_absent "${orders_sqlite}" "sqlx-postgres" "Orders SQLite adapter graph"
+
+service_postgres=$(cargo tree --locked \
+  -p orders-service \
+  --no-default-features \
+  --features postgres)
+require_contains "${service_postgres}" "sqlx-postgres" "Orders PostgreSQL service graph"
+require_absent "${service_postgres}" "sqlx-sqlite" "Orders PostgreSQL service graph"
+require_absent "${service_postgres}" "libsqlite3-sys" "Orders PostgreSQL service graph"
+
+service_sqlite=$(cargo tree --locked \
+  -p orders-service \
+  --no-default-features \
+  --features sqlite)
+require_contains "${service_sqlite}" "sqlx-sqlite" "Orders SQLite service graph"
+require_contains "${service_sqlite}" "libsqlite3-sys" "Orders SQLite service graph"
+require_absent "${service_sqlite}" "sqlx-postgres" "Orders SQLite service graph"
 
 printf '%s\n' 'SQLx PostgreSQL and SQLite feature graphs are isolated.'
